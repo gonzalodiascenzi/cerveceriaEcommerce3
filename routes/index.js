a36ac4aef380eb32141050c3ce44c85a5be2d300
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2021-01-29 09:16:00 -0300

    Cambios

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-
-  res.render ('index');
-
+router.get('/', function(req, res, next) {
+  res.render('index');
 });
+
+router.get('/contacto',  (req, res) => {
+  res.render('newViews/contacto');
+})
+
+router.get('/fundadores',  (req, res) => {
+  res.render('newViews/fundadores');
+})
+
+router.get('/eventos',  (req, res) => {
+  res.render('newViews/eventos');
+})
+
+router.get('/local',  (req, res) => {
+  res.render('newViews/local');
+})
+
 
 module.exports = router;
 
