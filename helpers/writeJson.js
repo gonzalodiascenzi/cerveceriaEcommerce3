e0f4646ec25b46f21d869da6719585fef8800701
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2021-02-04 09:46:09 -0300

    Se creo la carpeta helpers

---
+++ helpers/writeJson.js
@@ -0,0 +1,11 @@
+const readJson = require('./readJson');
+const filePath = require('./usersPath');
+const fs = require('fs');
+/////////////////////////////////////////////
+module.exports = (newData) => {
+    const allUsers = readJson();
+    const newUsers = [...allUsers, newData];
+    const newUsersJSON = JSON.stringify(newUsers, null, 2);
+    fs.writeFileSync(filePath, newUsersJSON);
+    return
+}
