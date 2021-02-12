0d6ee3f12380394228cc3c784c4cc1ddd08d90a1
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2021-02-12 19:40:23 -0300

    Cambios

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,6 +1,9 @@
 
 const fs = require('fs');
 const path = require('path');
+const productoPath = path.resolve(__dirname + '/../data/producto.json'); //Obtenemos la ruta de productos
+const productosJSON = fs.readFileSync(productoPath); //Leemos el archivo JSON
+const productoParseado = JSON.parse(productosJSON); //Parseamos el archivo JSON a un Array de Objetos Literales.
 
 module.exports= {
 
